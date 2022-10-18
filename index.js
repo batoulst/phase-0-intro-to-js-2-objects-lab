43d71afa3626b081db20948434eb3d2bb6c8653f
batoul sudki tenbakjy <batoul.tenbakji@gmail.com>
2022-10-18 16:03:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee={
+    name: 'batoul',
+    streetAddress: 'Mazza',
+};
+function updateEmployeeWithKeyAndValue(emp,key,val){
+return {...emp,[key]:val};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(emp,key,val){
+    emp[key]=val;
+    return emp;
+}
+function deleteFromEmployeeByKey(emp,key){
+    const nobj={...emp};
+    delete nobj[key];
+    return nobj;
+}
+function destructivelyDeleteFromEmployeeByKey(emp,key){
+    delete emp[key];
+    return emp;
+}
+
+x=deleteFromEmployeeByKey(employee,'name')
+console.log (x,employee)
